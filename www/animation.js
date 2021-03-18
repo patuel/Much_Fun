37c60eb0fa972054ac023e6cefb8b29785323465
patuel <paddy11211@googlemail.com>
2021-03-18 09:32:58 +0100

    We live in a capitalistic society, therefore I added more. More is good!

--- www/animation.js
+++ www/animation.js
@@ -1,13 +1,3 @@
-function loadImage(path) {
-    let img = document.createElement("img");
-    img.src = path;
-
-    return new Promise(resolve => {
-        img.addEventListener("load", () => resolve(img));
-        document.body.appendChild(img);
-    });
-};
-
 function toDegrees(deg) {
     return deg * Math.PI / 180;
 }
@@ -16,6 +6,8 @@ let canvas = document.getElementsByTagName("canvas")[0];
 
 let w = window.innerWidth;
 let h = window.innerHeight;
+let hw = w * .5;
+let hh = h * .5;
 
 canvas.width = w;
 canvas.height = h;
@@ -24,27 +16,63 @@ let ctx = canvas.getContext("2d");
 
 let sprite = null;
 
-let images = {
-    heart: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/heavy-black-heart_2764.png",
-    lmao: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/face-with-tears-of-joy_1f602.png",
-    pudding: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/custard_1f36e.png",
-    ferris: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/ferris-wheel_1f3a1.png",
-}
+let init = false;
 
-loadImage(images.lmao).then((img) => {
-    sprite = {
-        img,
-        pw: img.width,
-        ph: img.height,
-        hw: img.width * .5,
-        hh: img.height * .5
-    };
-    requestAnimationFrame(loop);
-});
+function setImage(emoji) {
+    ctx.font = "5em serif";
+    ctx.textBaseline = "top";
+
+    const dim = ctx.measureText(emoji);
+    console.log(dim);
+    let dimW = dim.width;
+    let hDimW = dimW * .5;
+    let dimH = dim.fontBoundingBoxDescent + dim.fontBoundingBoxAscent;
+    let hDimH = dimH * .5;
+
+    let tmpCanvas = document.createElement("canvas");
+    tmpCanvas.width = dimW;
+    tmpCanvas.height = dimH;
+
+    let tmpCtx = tmpCanvas.getContext("2d");
+    tmpCtx.font = "5em serif";
+    tmpCtx.textBaseline = "top";
+    // tmpCtx.fillStyle = "rgba(100,200,0,0.1)";
+    // tmpCtx.fillRect(0, 0, dimW, dimH);
+    tmpCtx.fillStyle = "red";
+    tmpCtx.fillText(emoji, 0, dim.actualBoundingBoxAscent);
+
+    let img = new Image();
+    img.width = dimW;
+    img.height = dimW;
+    img.src = tmpCanvas.toDataURL("image/png");
+
+    img.addEventListener("load", () => {
+        console.log(img);
+
+        deg = 0;
+        num = 1;
+        ctx.clearRect(0, 0, w, h);
+        sprite = {
+            img,
+            pw: dimW,
+            ph: dimH,
+            hw: hDimW,
+            hh: hDimH
+        };
+
+        if (!init) {
+            ctx.font = "2em serif";
+            ctx.fillStyle = "red";
+            ctx.fillText("⬅️ Change Emoji here ❤️", 150, 40);
+
+            init = true;
+            requestAnimationFrame(loop);
+        }
+    });
+};
+setImage("❤️");
 
 let deg = 0;
-let c = 0;
-
 let num = 1;
 const degadd = 0.005;
 const numadd = 0.5;
@@ -52,15 +80,20 @@ const numadd = 0.5;
 function loop() {
     // ctx.clearRect(0, 0, w, h);
     ctx.save();
-    ctx.translate(w*.5, h*.5);
 
-    const count = Math.floor(num);
-    let trans = (30 * Math.sin(num * 0.01)) + 15;
+    // ctx.drawImage(sprite.img, 200, 200);
+
+    ctx.translate(hw, hh);
+    ctx.rotate(toDegrees(num));
+
+    const count = Math.min(2000, Math.floor(num));
+    let trans = (30 * Math.sin(num * 0.02)) + 15;
     for (let i = 0; i < count; i++) {
         ctx.translate(trans, trans);
         // deg += degadd * 14;
-        ctx.rotate(toDegrees(i + deg));
+        ctx.rotate(toDegrees(i * 2 + deg));
         ctx.drawImage(sprite.img, -sprite.hw, -sprite.hw);
+        // ctx.fillText(text, 0, 0);
     }
     ctx.restore();
 
